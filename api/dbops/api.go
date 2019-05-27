f7002e3b9a79d9742cfdd5b227eb64975200aae2
jason <857615570@qq.com>
2019-05-27 15:16:32 +0800

    sub

--- api/dbops/api.go
+++ api/dbops/api.go
@@ -1,19 +1,23 @@
 package dbops
 
 import (
+	"time"
+	"log"
 	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
-	"log"
+	"github.com/lishengwei789/video_server/api/defs"
+	"github.com/lishengwei789/video_server/api/utils"
 )
 
 
-func AddUserCredential(loginName string,pwd string) error{
-	stmtIns ,err := dbConn.Prepare("insert into users (login_name,pwd) values (?,?)")
-	if err != nil{
+func AddUserCredential(loginName string, pwd string) error {
+	stmtIns, err := dbConn.Prepare("INSERT INTO users (login_name, pwd) VALUES (?, ?)")
+	if err != nil {
 		return err
 	}
-	_, err = stmtIns.Exec(loginName,pwd)
-	if err != nil{
+
+	_, err = stmtIns.Exec(loginName, pwd)
+	if err != nil {
 		return err
 	}
 
@@ -21,9 +25,9 @@ func AddUserCredential(loginName string,pwd string) error{
 	return nil
 }
 
-func GetUserCredential(loginName string) (string, error){
-	stmtOut ,err := dbConn.Prepare(" select pwd from users where login_name = ?")
-	if err != nil{
+func GetUserCredential(loginName string) (string, error) {
+	stmtOut, err := dbConn.Prepare("SELECT pwd FROM users WHERE login_name = ?")
+	if err != nil {
 		log.Printf("%s", err)
 		return "", err
 	}
@@ -39,13 +43,135 @@ func GetUserCredential(loginName string) (string, error){
 	return pwd, nil
 }
 
-func DeleteUser (LoginName string ,pwd string) error {
-	stmtDel ,err := dbConn.Prepare(" delete  from users where login_name = ? and pwd = ?")
-	if err != nil{
-		log.Printf("%s", err)
-		return  err
+func DeleteUser(loginName string, pwd string) error {
+	stmtDel, err := dbConn.Prepare("DELETE FROm users WHERE login_name=? AND pwd=?")
+	if err != nil {
+		log.Printf("DeleteUser error: %s", err)
+		return err
 	}
-	stmtDel.Exec(LoginName , pwd)
-	stmtDel.Close()
+
+	_, err = stmtDel.Exec(loginName, pwd)
+	if err != nil {
+		return err
+	}
+
+	defer stmtDel.Close()
 	return nil
 }
+
+func AddNewVideo(aid int, name string) (*defs.VideoInfo, error) {
+	// create uuid
+	vid, err := utils.NewUUID()
+	if err != nil {
+		return nil, err
+	}
+
+	t := time.Now()
+	ctime := t.Format("Jan 02 2006, 15:04:05")
+	stmtIns, err := dbConn.Prepare(`INSERT INTO video_info 
+		(id, author_id, name, display_ctime) VALUES(?, ?, ?, ?)`)
+	if err != nil {
+		return nil, err
+	}
+
+	_, err = stmtIns.Exec(vid, aid, name, ctime)
+	if err != nil {
+		return nil, err
+	}
+
+	res := &defs.VideoInfo{Id: vid, AuthorId: aid, Name: name, DisplayCtime: ctime}
+
+	defer stmtIns.Close()
+	return res, nil
+}
+
+
+func GetVideoInfo(vid string) (*defs.VideoInfo, error) {
+	stmtOut, err := dbConn.Prepare("SELECT author_id, name, display_ctime FROM video_info WHERE id=?")
+
+	var aid int
+	var dct string
+	var name string
+
+	err = stmtOut.QueryRow(vid).Scan(&aid, &name, &dct)
+	if err != nil && err != sql.ErrNoRows{
+		return nil, err
+	}
+
+	if err == sql.ErrNoRows {
+		return nil, nil
+	}
+
+	defer stmtOut.Close()
+
+	res := &defs.VideoInfo{Id: vid, AuthorId: aid, Name: name, DisplayCtime: dct}
+
+	return res, nil
+}
+
+func DeleteVideoInfo(vid string) error {
+	stmtDel, err := dbConn.Prepare("DELETE FROM video_info WHERE id=?")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmtDel.Exec(vid)
+	if err != nil {
+		return err
+	}
+
+	defer stmtDel.Close()
+	return nil
+}
+
+func AddNewComments(vid string, aid int, content string) error {
+	id, err := utils.NewUUID()
+	if err != nil {
+		return err
+	}
+
+	stmtIns, err := dbConn.Prepare("INSERT INTO comments (id, video_id, author_id, content) values (?, ?, ?, ?)")
+	if err != nil {
+		return err
+	}
+
+	_, err = stmtIns.Exec(id, vid, aid, content)
+	if err != nil {
+		return err
+	}
+
+	defer stmtIns.Close()
+	return nil
+}
+
+func ListComments(vid string, from, to int) ([]*defs.Comment, error) {
+	stmtOut, err := dbConn.Prepare(` SELECT comments.id, users.Login_name, comments.content FROM comments
+		INNER JOIN users ON comments.author_id = users.id
+		WHERE comments.video_id = ? AND comments.time > FROM_UNIXTIME(?) AND comments.time <= FROM_UNIXTIME(?)`)
+
+	var res []*defs.Comment
+
+	rows, err := stmtOut.Query(vid, from, to)
+	if err != nil {
+		return res, err
+	}
+
+	for rows.Next() {
+		var id, name, content string
+		if err := rows.Scan(&id, &name, &content); err != nil {
+			return res, err
+		}
+
+		c := &defs.Comment{Id: id, VideoId: vid, Author: name, Content: content}
+		res = append(res, c)
+	}
+	defer stmtOut.Close()
+
+	return res, nil
+}
+
+
+
+
+
+
--- api/defs/apidef.go
+++ api/defs/apidef.go
@@ -4,3 +4,10 @@ type UserCredential struct {
 	Username string `json:"user_name"`
 	Pwd string `json:"pwd"`
 }
+
+type VideoInfo struct {
+	Id string
+	AuthorId int
+	Name string
+	DisplayCtime string
+}
